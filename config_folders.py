ebeccbe7fd31583d07b48ad364d1749d9db1f3c8
lamoms954-dev <lamoms954@gmail.com>
2025-10-12 15:12:13 +0200

    Update config_folders.py

--- config_folders.py
+++ config_folders.py
@@ -6,13 +6,11 @@ SORTED_DIR = os.path.join("tickets_scanner")      # Dossier final (classé)
 
 # Dossier data interne
 DATA_DIR = os.path.join("data")
-RAW_DIR = os.path.join(DATA_DIR, "raw_tickets")
-SCANNED_DIR = os.path.join(DATA_DIR, "scanned_tickets")
 
 # Dossiers revenus
 REVENUS_A_TRAITER = os.path.join("revenus_a_traiter")
 REVENUS_TRAITE = os.path.join("revenus_traités")
 
 # Création automatique de tous les dossiers
-for d in [TO_SCAN_DIR, SORTED_DIR, DATA_DIR, RAW_DIR, SCANNED_DIR, REVENUS_A_TRAITER, REVENUS_TRAITE]:
+for d in [TO_SCAN_DIR, SORTED_DIR, DATA_DIR, REVENUS_A_TRAITER, REVENUS_TRAITE]:
     os.makedirs(d, exist_ok=True)
